14c9e1b345c0c97a64a1907e4795c794649b0d30
Taras Ivashchuk <tvishy@disroot.org>
2025-11-17 12:25:03 +0200

    Refactored

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from typing import Hashable, Any, Union
 
@@ -27,26 +28,12 @@ class Dictionary:
             self.threshold = int(self.capacity * self.load_factor)
             new_nodes = [None] * self.capacity
             for node in self.nodes:
-                if not self.__is_empty(node) and not self.__is_deleted(node):
-                    rehashed_node = Node(
-                        node.node_key,
-                        hash(node.node_key),
-                        node.node_value
-                    )
-                    self.__add_node(new_nodes, rehashed_node)
+                if node is not None and node is not DELETED:
+                    self.__add_node(new_nodes, node)
             self.nodes = new_nodes
         new_node = Node(key, hash(key), value)
-        # what will happen if add_node returns false? Will capacity increase?
         if self.__add_node(self.nodes, new_node):
             self.size += 1
-
-    @staticmethod
-    def __is_empty(node: Node) -> bool:
-        return node is None
-
-    @staticmethod
-    def __is_deleted(node: Node) -> bool:
-        return node is DELETED
 
     def __add_node(
             self,
@@ -60,50 +47,46 @@ class Dictionary:
 
         slot = node.node_hash % len(nodes)
 
-        # 1. Add the new node
-        if self.__is_empty(nodes[slot]) or self.__is_deleted(nodes[slot]):
+        if nodes[slot] is None or nodes[slot] is DELETED:
             nodes[slot] = node
             return True
-        # 2. Overwrite the old value
         if (node.node_hash == nodes[slot].node_hash
                 and node.node_key == nodes[slot].node_key):
             nodes[slot].node_value = node.node_value
             return False
-        # 3. Collision detected
         if nodes[slot].node_key != node.node_key:
-            # find an empty slot
-            new_slot_inx = (slot + 1) % len(nodes)
+            new_slot = (slot + 1) % len(nodes)
             for _ in range(len(nodes)):
-                # insert into empty slot
-                if (self.__is_empty(nodes[new_slot_inx])
-                        or self.__is_deleted(nodes[new_slot_inx])):
-                    nodes[new_slot_inx] = node
+                if (nodes[new_slot] is None
+                        or nodes[new_slot] is DELETED):
+                    nodes[new_slot] = node
                     return True
                 # overwrite the old value
-                if nodes[new_slot_inx].node_key == node.node_key:
-                    nodes[new_slot_inx].node_value = node.node_value
+                if (nodes[new_slot].node_hash == node.node_hash
+                        and nodes[new_slot].node_key == node.node_key):
+                    nodes[new_slot].node_value = node.node_value
                     return False
 
-                new_slot_inx = (new_slot_inx + 1) % len(nodes)
+                new_slot = (new_slot + 1) % len(nodes)
             else:
                 raise Exception(f"Size: {len(nodes)} no empty slots found")
         return False
 
-    def __getitem__(self, obj: Any) -> Any:
-        key_hash = hash(obj)
+    def __getitem__(self, key: Any) -> Any:
+        key_hash = hash(key)
         slot = key_hash % self.capacity
 
         for _ in range(self.capacity):
-            if self.__is_empty(self.nodes[slot]):
-                raise KeyError(f"{obj} is not found in dictionary")
-            if self.__is_deleted(self.nodes[slot]):
+            if self.nodes[slot] is None:
+                raise KeyError(f"{key} is not found in dictionary")
+            if self.nodes[slot] is DELETED:
                 slot = (slot + 1) % self.capacity
                 continue
             if (self.nodes[slot].node_hash == key_hash
-                    and self.nodes[slot].node_key == obj):
+                    and self.nodes[slot].node_key == key):
                 return self.nodes[slot].node_value
             slot = (slot + 1) % self.capacity
-        raise KeyError(f"{obj} is not found in dictionary")
+        raise KeyError(f"{key} is not found in dictionary")
 
     def __len__(self) -> int:
         return self.size
@@ -114,38 +97,47 @@ class Dictionary:
         self.threshold = int(self.capacity * self.load_factor)
         self.nodes = [None] * self.capacity
 
-    def __delitem__(self, obj: Any) -> None:
-        key_hash = hash(obj)
+    def __delitem__(self, key: Any) -> None:
+        key_hash = hash(key)
         slot = key_hash % self.capacity
 
         for _ in range(self.capacity):
-            if self.__is_empty(self.nodes[slot]):
-                raise KeyError(f"{obj} is not found in dictionary")
-            if self.__is_deleted(self.nodes[slot]):
+            if self.nodes[slot] is None:
+                raise KeyError(f"{key} is not found in dictionary")
+            if self.nodes[slot] is DELETED:
                 slot = (slot + 1) % self.capacity
                 continue
-            if (self.nodes[slot].node_hash == hash(obj)
-                    and self.nodes[slot].node_key == obj):
+            if (self.nodes[slot].node_hash == key_hash
+                    and self.nodes[slot].node_key == key):
                 self.nodes[slot] = DELETED
                 self.size -= 1
                 break
             slot = (slot + 1) % self.capacity
         else:
-            raise KeyError(f"{obj} is not found in dictionary")
+            raise KeyError(f"{key} is not found in dictionary")
 
-    def get(self, node: Node, default: Any = None) -> Any:
+    def get(self, key: Any, default: Any = None) -> Any:
         try:
-            return self.__getitem__(node)
+            return self.__getitem__(key)
         except KeyError:
             return default
 
-    def pop(self, obj: Any, default: Any = None) -> Any:
+    def pop(self, key: Any, default: Any = None) -> Any:
         try:
-            value = self.get(obj, default)
-            self.__delitem__(obj)
+            value = self.get(key, default)
+            self.__delitem__(key)
             return value
         except KeyError:
             if default is not None:
                 return default
             else:
                 raise
+
+    def __iter__(self) -> Any:
+        return self.nodes
+
+    def update(self, other: Dictionary) -> None:
+        for node in other.nodes:
+            if node:
+                self.__setitem__(node.node_key, node.node_value)
+            ...
